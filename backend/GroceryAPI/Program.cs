9ce88a0c492b31409b346ee66a828b82d2affff1
LINHTK <trankhanhlinh2201004@gmail.com>
2025-06-05 01:23:40 +0700

    Backend: Add DI dependencies

--- backend/GroceryAPI/Program.cs
+++ backend/GroceryAPI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessObjects;
+using DataAccess.DAO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using Repositories.Interfaces;
@@ -23,15 +24,28 @@ builder.Services.AddCors(options =>
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// --- DAOs: Direct database access ---
+builder.Services.AddScoped<CustomerDAO>();
+builder.Services.AddScoped<EmployeeDAO>();
+builder.Services.AddScoped<OrderDAO>();
+builder.Services.AddScoped<OrderDetailDAO>();
+builder.Services.AddScoped<ProductDAO>();
+builder.Services.AddScoped<RetailOutletDAO>();
+builder.Services.AddScoped<SupplierDAO>();
+builder.Services.AddScoped<WarehouseDAO>();
 
+// --- Repositories: Business logic layer ---
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IRetailOutletRepository, RetailOutletRepository>();
 builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
 builder.Services.AddScoped<IWarehouseRepository, WarehouseRepository>();
 
+// --- Utilities: Supporting services ---
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 //builder.Services.AddControllers();
 builder.Services.AddControllers()
@@ -48,7 +62,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Đường dẫn tới thư mục React build
-var reactDistPath = Path.Combine(Directory.GetCurrentDirectory(), "GroceryUI", "dist");
+//var reactDistPath = Path.Combine(Directory.GetCurrentDirectory(), "GroceryUI", "dist");
+// Giả sử GroceryUI nằm cùng cấp GroceryAPI
+var reactDistPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "GroceryUI", "dist");
 
 app.UseStaticFiles(new StaticFileOptions
 {
