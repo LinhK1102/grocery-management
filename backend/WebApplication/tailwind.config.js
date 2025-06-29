1f9f4ed1da4a50271a8d4a4442dbb1568799668a
LINHTK <trankhanhlinh2201004@gmail.com>
2025-06-29 22:54:42 +0700

    Backend: webapp ui first update

---
+++ backend/WebApplication/tailwind.config.js
@@ -0,0 +1,17 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.{cshtml,html}',           // Razor Pages
+        './Views/**/*.{cshtml,html}',           // Nếu có Views
+        './wwwroot/js/**/*.js',                 // JS trong wwwroot (nếu dùng)
+        './node_modules/flowbite/**/*.js',       // Flowbite components
+        "./wwwroot/**/*.cshtml"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('daisyui'),                     // DaisyUI plugin
+        require('flowbite/plugin')              // Flowbite plugin
+    ],
+}
